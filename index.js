5edf1937bb2d7a29827176fef184ea5d4637a531
Nils Matic <matic1909@gmail.com>
2023-05-04 16:08:08 +0200

    added 16x16 grid generated via JS, basic styling

---
+++ index.js
@@ -0,0 +1,14 @@
+const gridContainer = document.querySelector('.grid-container');
+
+function generateGrid(size) {
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < size * size; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    gridContainer.appendChild(cell);
+  }
+}
+
+window.onload = () => {
+  generateGrid(16);
+};
