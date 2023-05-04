f058e53bfd0c9863b3800af31eeff0d53538cb08
Nils Matic <matic1909@gmail.com>
2023-05-04 16:30:52 +0200

    added EventListener to each cell to color it in

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function generateGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.addEventListener('mouseenter', (e) => cell.classList.add('filled'));
     gridContainer.appendChild(cell);
   }
 }
