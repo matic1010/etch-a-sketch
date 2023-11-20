a418b024d1706109fb6a3a3ade4b4b3d72ecbbf7
Nils Matic <matic1909@gmail.com>
2023-11-20 13:14:05 +0100

    added button for generating a new grid

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const grid = document.getElementById("grid")
+const newGridButton = document.getElementById('new-grid-button')
+let gridSize = 4
 
-for(let i = 0; i < 16; i++) {
-  const gridItem = document.createElement('div');
-  gridItem.addEventListener('mouseover', (e) => {
-    gridItem.classList.add('colored');
-  })
-  gridItem.classList.add('grid-item')
-  grid.appendChild(gridItem)
+newGridButton.addEventListener("click", () => {
+  let gridSize = undefined;
+  while(!gridSize) {
+    gridSize = Number(prompt("How many squares per side?"))
+  }
+  generateGrid(gridSize)
+})
+
+function generateGrid(size) {
+  grid.innerHTML = ''
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  for(let i = 0; i < size * size; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.addEventListener('mouseover', (e) => {
+      gridItem.classList.add('colored');
+    })
+    gridItem.classList.add('grid-item')
+    grid.appendChild(gridItem)
+  }
 }
+
+generateGrid(gridSize)
