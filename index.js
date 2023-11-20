2dcd9d5394ed65d85e55009b819da062d03fcf3c
Nils Matic <matic1909@gmail.com>
2023-11-20 13:34:57 +0100

    added random mode and mode selector

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const grid = document.getElementById("grid")
 const newGridButton = document.getElementById('new-grid-button')
+const modeSelector = document.getElementById('modes')
+
 let gridSize = 4
+
+modeSelector.addEventListener("change", () => generateGrid(gridSize))
 
 newGridButton.addEventListener("click", () => {
   let gridSize = undefined;
@@ -16,11 +20,23 @@ function generateGrid(size) {
   for(let i = 0; i < size * size; i++) {
     const gridItem = document.createElement('div');
     gridItem.addEventListener('mouseover', (e) => {
-      gridItem.classList.add('colored');
-    })
+      if(modeSelector.value === "random") {
+        gridItem.style.backgroundColor = getRandomRGB();
+      } else {
+        gridItem.style.backgroundColor = "black";
+      }
+    }, { once: true })
     gridItem.classList.add('grid-item')
     grid.appendChild(gridItem)
   }
+}
+
+function getRandomRGB() {
+  const rValue = Math.floor(Math.random() * 255)
+  const gValue = Math.floor(Math.random() * 255)
+  const bValue = Math.floor(Math.random() * 255)
+
+  return `rgb(${rValue}, ${gValue}, ${bValue})`
 }
 
 generateGrid(gridSize)
